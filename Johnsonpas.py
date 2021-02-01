ad34e628a43de937692d06d7db1275fa2a3d5328
iloveabby <78292152+iloveabby@users.noreply.github.com>
2021-01-31 19:23:44 -0800

    Update Johnsonpas.py

--- Johnsonpas.py
+++ Johnsonpas.py
@@ -1,14 +1,27 @@
 """
-this python script is a extremly crude password mananger
-i'm hoping to eventualy make a login system
-Aswell as a encrypted passwordfile
-plese dont kill me for this I'm just starting out.
-
+This is the Second version of this script. I've added a basic login system. It's nothing impressive.
+I'm hoping to optimize this soon. I think I can make it a little more light weight.
+I dont like bloat. Maybe someday I will set up a encryption system.
 
 """
+
+
+
+import os.path
+loginfile = os.path.exists('login.txt')
+if loginfile == False:
+    login = open('login.txt','w')
+    login.write(input('You do not have a login password.Type your login passowrd'))
+    login.close()
+
+login = open('login.txt','r').read()
+userlogin = input('Type your login password')
+
+if userlogin != login:
+    print('password wrong')
+    exit() 
 while True:
     user = input('r to read passwords w to save, x to break')
-
     if user == 'w':
         passdir = input('Type the filepath and press enter. .')
         password = open(passdir, 'a')
@@ -21,7 +34,7 @@ while True:
     elif user == 'r':
         dir = input('Please type the the file path of the passwordfile.')
         passr = open(dir, 'r').read()
-        print (passr)
-        
+        print(passr)
     elif user == 'x':
         break
+
